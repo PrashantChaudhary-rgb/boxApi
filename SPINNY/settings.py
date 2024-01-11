38a89a0ce162934b5d1e3fb8b01c43d423d6b5b6
PrashantChaudhary-rgb <73767749+PrashantChaudhary-rgb@users.noreply.github.com>
2024-01-11 12:26:30 +0530

    added requirements

--- SPINNY/settings.py
+++ SPINNY/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-d9^##p)z9@^_=37p)t*e+!xeb-^n@m&j97@)ue%qtfc#lzers^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://spinny-dnui.onrender.com/', 'http://127.0.0.1/']
 
 
 # Application definition
