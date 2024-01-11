9d5203dc5929412dc587a5df0cf3d8a1269a452c
PrashantChaudhary-rgb <73767749+PrashantChaudhary-rgb@users.noreply.github.com>
2024-01-11 12:04:37 +0530

    getting ready for deployement

--- SPINNY/settings.py
+++ SPINNY/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,10 +89,7 @@ WSGI_APPLICATION = 'SPINNY.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	"default": dj_database_url.parse('postgres://spinny_user:iCxknrS2SG2sp6lodI4Lh3iN4Mni484e@dpg-cmfoa60cmk4c739cve10-a.oregon-postgres.render.com/spinny')
 }
 
 
--- boxstore/views.py
+++ boxstore/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from django.db.models import Avg, Count
 from django.utils import timezone
@@ -12,27 +11,27 @@ from .filters import BoxFilter
 class BoxList(generics.ListCreateAPIView):
     queryset = Box.objects.all()
     serializer_class = BoxSerializer
-    permission_classes = [IsStaffOrReadOnly ]
+    permission_classes = [IsStaffOrReadOnly]
     filterset_class = BoxFilter
-
+    print(queryset)
    
     def perform_create(self, serializer):
         # Additional conditions before creating a new box
         # Check average area, average volume, and total boxes added in a week
         # Update these values based on A1, V1, L1, and L2 configurations
     
-
+        print("performing create")
         # Check average area
         avg_area = Box.objects.all().aggregate(avg_area=Avg('area'))['avg_area'] or 0
         curr_area = self.request.data['length'] * self.request.data['breadth']
-        if avg_area + curr_area > 100:
-           return Response({"detail": "Average area exceeds the limit A1"}, status=status.HTTP_400_BAD_REQUEST)
-
+        #if avg_area + curr_area > 100:
+        #   return Response({"detail": "Average area exceeds the limit A1"}, status=status.HTTP_400_BAD_REQUEST)
+        print("performing create")
         # Check average volume
-        avg_volume = Box.objects.all().aggregate(avg_volume=Avg('volume'))['avg_volume'] or 0
+        #avg_volume = Box.objects.all().aggregate(avg_volume=Avg('volume'))['avg_volume'] or 0
         curr_vol = self.request.data['length'] * self.request.data['breadth']*self.request.data['height']
-        if avg_volume + serializer.validated_data['volume'] > 1000:
-            return Response({"detail": "Average volume exceeds the limit V1"}, status=status.HTTP_400_BAD_REQUEST)
+        #if avg_volume + serializer.validated_data['volume'] > 1000:
+        #    return Response({"detail": "Average volume exceeds the limit V1"}, status=status.HTTP_400_BAD_REQUEST)
 
         # Check total boxes added in a week
         week_start = timezone.now() - timezone.timedelta(days=7)
@@ -44,7 +43,7 @@ class BoxList(generics.ListCreateAPIView):
         user_total_boxes_in_week = Box.objects.filter(created_at__gte=week_start, created_by=self.request.user).count()
         if user_total_boxes_in_week >= 50:
             return Response({"detail": "Total boxes added by the user in a week exceeds the limit L2"}, status=status.HTTP_400_BAD_REQUEST)
-        print(self.request)
+        print("creating and saving")
         serializer.save(created_by=self.request.user, area = curr_area, volume = curr_vol)
 
 class BoxDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -68,8 +67,9 @@ class MyBoxList(generics.ListAPIView):
     serializer_class = BoxSerializer
     permission_classes = [IsStaffOrReadOnly]
     filterset_class = BoxFilter
-
+    print("hii")
     def get_queryset(self):
+        print("hello")
         return Box.objects.filter(created_by=self.request.user)
 
 class BoxDelete(generics.DestroyAPIView):
